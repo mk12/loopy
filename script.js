995948c8441f24be086d99b4179d8ac3527dc98a
Mitchell Kember <mk12360@gmail.com>
2023-11-04 11:00:38 -0700

    Working on canvas sizing

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // Copyright 2023 Mitchell Kember. Subject to the MIT License.
 
-window.addEventListener("load", () => {
-    const canvas = document.getElementById("Output");
-    const ctx = canvas.getContext("2d");
-
-    let i = 5;
-    const resizeObserver = new ResizeObserver(() => {
-        resizeObserver.disconnect();
-        const { width, height } = canvas.getBoundingClientRect();
-        canvas.width = width * window.devicePixelRatio;
-        canvas.height = height * window.devicePixelRatio;
-        // ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+addEventListener("load", () => {
+    const canvas = document.getElementById("Canvas");
+    const ctx = canvas.getContext("2d", { alpha: false });
+    let N = 0;
+    new ResizeObserver((entries) => {
+        const newRect = entries[0].contentRect;
+        const w = Math.floor(newRect.width);
+        const h = Math.floor(newRect.height);
+        if (canvas.width === w && canvas.height === h) return;
+        canvas.width = w;
+        canvas.height = h;
         ctx.font = "20px serif";
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText(`${canvas.width}x${canvas.height}`, 10, 25);
-        // ctx.fillRect(i, i, i + 10, i + 10);
-        // i = (i + 1) % (canvas.width - 20);
-        // resizeObserver.observe(canvas);
-    });
-    // resizeObserver.observe(canvas)
+        ctx.clearRect(0, 0, w, h);
+        ctx.fillStyle = "#aaa";
+        ctx.fillRect(0, 0, w, h);
+        ctx.fillStyle = "#000"
+        ctx.fillText(`[${N++}] ${w}x${h}`, 10, 25);
+    }).observe(canvas);
 });
